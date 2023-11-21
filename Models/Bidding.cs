507c9f990a6ba2687fe9efd1a4964a87f671c2e6
Leonardo Cruz <leonardo.m.cruz@hotmail.com>
2023-11-21 18:57:08 -0300

    Added bidding model

---
+++ Models/Bidding.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace NpTest.Models;
+
+public class Bidding
+{
+    public int Id { get; set; }
+    [Required]
+    public int Number { get; set; }
+    public string? Description { get; set; }
+    public DateTime OpenDate { get; set; }
+    [Required]
+    public BiddingStatus Status { get; set; }
+}
+
+public enum BiddingStatus
+{
+    Open,
+    InProgress,
+    Closed
+}
