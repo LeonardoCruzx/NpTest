f3d0307d100a940edb5d6a4d507506ee2d23aeb0
Leonardo Cruz <leonardo.m.cruz@hotmail.com>
2023-11-22 16:11:58 -0300

    Added bidding seed and first migrations

--- Data/BiddingContext.cs
+++ Data/BiddingContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using NpTest.Models;
 
 namespace NpTest.Data;
 
@@ -6,5 +7,57 @@ public class BiddingContext : DbContext
 {
     public BiddingContext(DbContextOptions<BiddingContext> options) : base(options)
     {
+    }
+
+    public DbSet<Bidding> Biddings { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        ConfigureBidding(modelBuilder);
+        SeedBiddings(modelBuilder);
+
+
+    }
+
+    private void ConfigureBidding(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Bidding>().ToTable("Bidding");
+        modelBuilder.Entity<Bidding>().HasKey(b => b.Id);
+        modelBuilder.Entity<Bidding>().Property(b => b.Id).IsRequired().ValueGeneratedOnAdd();
+        modelBuilder.Entity<Bidding>().Property(b => b.Number).IsRequired();
+        modelBuilder.Entity<Bidding>().Property(b => b.Description).IsRequired(false);
+        modelBuilder.Entity<Bidding>().Property(b => b.OpenDate).IsRequired();
+        modelBuilder.Entity<Bidding>().Property(b => b.Status).IsRequired();
+
+    }
+
+    private void SeedBiddings(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Bidding>().HasData(
+            new Bidding
+            {
+                Id = 1,
+                Number = 1,
+                Description = "First bidding",
+                OpenDate = DateTime.Now,
+                Status = BiddingStatus.Open
+            },
+            new Bidding
+            {
+                Id = 2,
+                Number = 2,
+                Description = "Second bidding",
+                OpenDate = DateTime.Now,
+                Status = BiddingStatus.Open
+            },
+            new Bidding
+            {
+                Id = 3,
+                Number = 3,
+                Description = "Third bidding",
+                OpenDate = DateTime.Now,
+                Status = BiddingStatus.Open
+            }
+        );
     }
 }
---
+++ Migrations/20231121215604_First migration.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace NpTest.Migrations
+{
+    /// <inheritdoc />
+    public partial class Firstmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bidding",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Number = table.Column<int>(type: "INTEGER", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    OpenDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bidding", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Bidding",
+                columns: new[] { "Id", "Description", "Number", "OpenDate", "Status" },
+                values: new object[,]
+                {
+                    { 1, "First bidding", 1, new DateTime(2023, 11, 21, 18, 56, 3, 556, DateTimeKind.Local).AddTicks(7850), 0 },
+                    { 2, "Second bidding", 2, new DateTime(2023, 11, 21, 18, 56, 3, 556, DateTimeKind.Local).AddTicks(7860), 0 },
+                    { 3, "Third bidding", 3, new DateTime(2023, 11, 21, 18, 56, 3, 556, DateTimeKind.Local).AddTicks(7861), 0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bidding");
+        }
+    }
+}
