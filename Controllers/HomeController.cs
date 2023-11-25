138227f052fc7c0e5afddb9da2dc625fb2cb08ee
Leonardo Cruz <leonardo.m.cruz@hotmail.com>
2023-11-25 20:05:39 -0300

    Created order by and filter by for bidding list

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using NpTest.Models;
 using NpTest.Services.Interfaces;
+using NpTest.ViewModels;
 
 namespace NpTest.Controllers;
 
@@ -10,11 +12,38 @@ public class HomeController(ILogger<HomeController> logger, IBiddingService bidd
     private readonly ILogger<HomeController> _logger = logger;
     private readonly IBiddingService _biddingService = biddingService;
 
-    public async Task<IActionResult> Index()
+    public async Task<IActionResult> Index(HomeViewModel model)
     {
         var biddings = await _biddingService.GetBiddingsAsync();
 
-        return View(biddings);
+        if (!string.IsNullOrEmpty(model.FilterBy) && !string.IsNullOrEmpty(model.FilterByValue))
+        {
+            biddings = biddings.Where(b => b.GetType().GetProperty(model.FilterBy)?.GetValue(b)?.ToString()?.Contains(model.FilterByValue, StringComparison.OrdinalIgnoreCase) ?? false);
+        }
+
+        if (!string.IsNullOrEmpty(model.OrderBy) && !string.IsNullOrEmpty(model.OrderByType))
+        {
+            biddings = model.OrderByType switch
+            {
+                "asc" => biddings.OrderBy(b => b.GetType().GetProperty(model.OrderBy)?.GetValue(b)),
+                "desc" => biddings.OrderByDescending(b => b.GetType().GetProperty(model.OrderBy)?.GetValue(b)),
+                _ => biddings
+            };
+        }
+
+        var biddingFields = typeof(Bidding).GetProperties().Where(p => p.PropertyType.IsValueType || p.PropertyType == typeof(string)).Select(p => p.Name);
+        ViewBag.Fields = new SelectList(biddingFields);
+
+        var viewModel = new HomeViewModel
+        {
+            Biddings = biddings,
+            OrderBy = model.OrderBy,
+            OrderByType = model.OrderByType,
+            FilterBy = model.FilterBy,
+            FilterByValue = model.FilterByValue
+        };
+
+        return View(viewModel);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
