3533793d741d8f699769956f63c9331944c66994
Leonardo Cruz <leonardo.m.cruz@hotmail.com>
2023-11-25 17:25:56 -0300

    Added bidding edit

--- Controllers/BiddingController.cs
+++ Controllers/BiddingController.cs
@@ -2,8 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using NpTest.Data;
 using NpTest.Models;
-using NpTest.ViewModels;
-
 namespace NpTest.Controllers;
 
 public class BiddingController(ILogger<BiddingController> logger, BiddingContext context) : Controller
@@ -52,6 +50,46 @@ public class BiddingController(ILogger<BiddingController> logger, BiddingContext
         await _context.SaveChangesAsync();
 
         return RedirectToAction("Index", "Home");
+    }
+
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var bidding = await _context.Biddings.FindAsync(id);
+        if (bidding == null)
+        {
+            return NotFound();
+        }
+
+        ViewBag.Statuses = new SelectList(Enum.GetValues(typeof(BiddingStatus)));
+        return View(bidding);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id, Number, Description, Status")] Bidding model)
+    {
+        ViewBag.Statuses = new SelectList(Enum.GetValues(typeof(BiddingStatus)));
+
+        if (id != model.Id)
+        {
+            return NotFound();
+        }
+
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        _context.Update(model);
+        await _context.SaveChangesAsync();
+        
+        ViewBag.SuccessMessage = "Bidding updated successfully!";
+        return View(model);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
